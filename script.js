04de5c973afb2b467373a5ecd9a4364049694afb
NunoMCP <NunoMCP@users.noreply.github.com>
2020-09-22 17:37:00 +0100

    finish

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var taskListItems = document.getElementsByTagName("LI");
 
 for(var i = 0; i < taskListItems.length; i++){
     var span = document.createElement("span")
-    var text = document.createTextNode("\u00D7")
+    var x = document.createTextNode("\u00D7")
     span.className = "close"
-    span.appendChild(text)
+    span.appendChild(x)
     taskListItems[i].appendChild(span)
 }
 
@@ -25,3 +25,29 @@ list.addEventListener("click", function(ev){
     }
 }, false)
 
+function createTask() {
+    var taskListNew = document.createElement("li")
+    inputValue = document.getElementById("textInput").value
+    var taskText = document.createTextNode(inputValue)
+    taskListNew.appendChild(taskText)
+    if(inputValue === ""){
+        alert("it must not be empty")
+    }else {
+        var mainTaskList = document.getElementById("taskList")
+        mainTaskList.appendChild(taskListNew)
+    }
+    document.getElementById("textInput").value = ""
+
+    var span = document.createElement("span")
+    var x = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(x)
+    taskListNew.appendChild(span)
+
+    for(var i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
